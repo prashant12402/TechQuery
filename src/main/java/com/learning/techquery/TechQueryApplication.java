dd7aa300b1c4b422a195d30752886605615bbac9
Prashant Pathak <prashantpathak12402@gmail.com>
2024-07-28 16:07:57 +0530

    Initial commit

---
+++ src/main/java/com/learning/techquery/TechQueryApplication.java
@@ -0,0 +1,13 @@
+package com.learning.techquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/techquery/TechQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.techquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
